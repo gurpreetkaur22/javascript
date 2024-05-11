21250b84cb830b090d1602e88bdcabb165bcda61
gurpreetkaur22 <112333393+gurpreetkaur22@users.noreply.github.com>
2024-05-11 19:56:54 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,31 @@
+"use strict"; // treat all JS code as newer version
+
+// alert(3+5) // we are using nodejs, not browser
+
+console.log(3
+    +3) // code readability should be high
+
+console.log("gopi");
+
+let name = "gurpreet"
+let age = 20
+let loggedIn = true
+let state;
+
+
+// primitve datatypes:
+
+// number = 2 to power 53
+// bingInt
+// string => ""
+// boolean => true/false
+// null => standalone value 
+// undefined
+// symbol = unique
+
+// object
+
+console.log(typeof name); // string
+console.log(typeof age); // number
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
